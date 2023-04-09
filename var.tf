ac9d33917b54bdc1446a57b82d8566959d02db77
alxshhvcv <127872644+alxshhvcv@users.noreply.github.com>
2023-04-09 21:17:16 +0300

    part 2

--- var.tf
+++ var.tf
@@ -10,3 +10,15 @@ variable "users" {
 variable "nodes_name" {
   default = ["User1-cx11", "User2-cx11", "User3-cx11"]
 }
+
+variable "ssh_keys" {
+  description = "List of ssh keys"
+}
+
+variable "servers_list" {
+  description = "List of servers"
+}
+
+variable "base_packages" {
+  default = ["htop", "mc", "git", "fail2ban", "curl", "apt-transport-https", "ufw"]
+}
